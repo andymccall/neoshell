d5ea4612473dea7666a079d866119cc5066484ff
Andy McCall <mailme@andymccall.co.uk>
2025-06-24 19:08:51 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,47 @@
+# ----------------------------
+# Makefile Options
+# ----------------------------
+
+NAME       = ns
+CA65       = ca65
+AR65       = ar65
+CL65       = cl65
+RM         = rm -f
+NEOEMU     = neo
+NEO_HOME   = ~/development/tools/neo6502/
+
+default: all
+
+all:
+	@echo "Building project..."
+	mkdir -p bin
+	mkdir -p lst
+	mkdir -p map
+	$(CL65) --static-locals -t none -C src/includes/system/neo6502.cfg -O --cpu 65c02 -l lst/$(NAME).lst -m map/$(NAME).map -o bin/$(NAME).bin src/main.asm src/includes/system/neo6502.lib
+	python3 $(NEO_HOME)exec.zip bin/$(NAME).bin@800 run@800 -o"bin/$(NAME).neo"
+	rm bin/$(NAME).bin
+
+clean:
+	@echo "Cleaning project..."
+	rm -rf bin
+	rm -rf lst
+	rm -rf map
+	rm -rf *.bin
+	rm -rf storage
+	if [ -f memory.dump ]; then rm memory.dump; fi
+
+run:
+	@echo "Launching emulator..."
+	mkdir -p storage
+	cp bin/$(NAME).neo storage
+	$(NEOEMU) bin/$(NAME).neo cold
+	rm -rf storage
+	rm memory.dump
+
+run-debug:
+	@echo "Launching emulator..."
+	mkdir -p storage
+	cp bin/$(NAME).neo storage
+	$(NEOEMU)
+	rm -rf storage
+	rm memory.dump
---
+++ src/main.asm
@@ -0,0 +1,51 @@
+.include "includes/system/neo6502.asm"
+
+.org $800
+.segment "STARTUP"
+
+   jmp start
+
+; API includes
+   .include "includes/api/br_text.inc"
+   .include "includes/api/br_screen.inc"
+   .include "includes/api/br_graphics.inc"
+   .include "includes/api/br_debug.inc"
+
+; App includes
+   .include "includes/app/constants.inc"
+   .include "includes/app/prompt.inc"
+   .include "includes/app/version.inc"
+   .include "includes/app/input.inc"
+   .include "includes/app/parse.inc"
+   .include "includes/app/commands.inc"
+   .include "includes/app/clear.inc"
+   .include "includes/app/exit.inc"
+
+;---------------------------------------------------------------
+; Menu equates
+;---------------------------------------------------------------
+;
+start:
+
+   jsr br_screen_clear
+
+   jsr print_version_info
+   jsr br_text_print_new_line
+
+app_loop:
+  
+   jsr print_prompt
+
+   jsr get_input
+
+   ;lda #<INPUT_BUFFER
+   ;sta ZP_PTR_LO
+   ;lda #>INPUT_BUFFER
+   ;sta ZP_PTR_HI
+   ;jsr br_text_print_string_nl
+   
+   jsr parse_input
+
+done:
+   jmp app_loop
+
